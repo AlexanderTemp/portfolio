cadbd5c53baf756041338598c2732f4a11d3ebab
attemptx <aaalex2025@gmail.com>
2026-02-09 16:29:39 -0400

    feat: initial project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import HeroImage from './modules/main/HeroImage.vue'
+</script>
+
+<template>
+  <main>
+    <div class="h-dvh grid grid-cols-2 gap-10">
+      <div class="flex flex-col">
+        <h1 class="text-primary font-bold">
+          <span class="text-secondary">Hola 👋</span>&nbsp;Soy Alexander
+        </h1>
+        <h3 class="text-secondary">Desarrollador Fullstack</h3>
+
+        <p class="text-primary">
+          Desarrollo proyectos a medida que cuenten con funcionalidad, seguros, despliegue rápido y
+          con una buena experiencia de usuario.
+        </p>
+      </div>
+      <hero-image class="w-full h-full flex justify-center items-center" />
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
